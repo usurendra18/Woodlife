39da2b20c94a073033a221153b63b81b959b77ac
usurendra18 <Surendra7Darling@gmail.com>
2025-01-26 20:39:09 +0530

    Update Stremapp.py

--- Stremapp.py
+++ Stremapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-DATA_PATH = "C://Users//91964//myproject//Candiour 40.xlsx"
+DATA_PATH = "Woodlife/Candiour 40.xlsx"
 df = pd.read_excel(DATA_PATH)
 
 # Filters
